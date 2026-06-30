55293c7c8ec6536aa0fc1d1150df10c556552260
David Rabko <david.rabko@icloud.com>
2026-06-30 11:09:38 +0300

    Update the nvim theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,20 @@
 return {
   {
-    "kvrohit/mellow.nvim",
-  },
-  {
-    "LazyVim/LazyVim",
+    "rebelot/kanagawa.nvim",
+    priority = 1000, -- Приоритет для загрузки до других плагинов
+    lazy = false,    -- Загружать сразу, чтобы не нарушать фон
     opts = {
-      colorscheme = "mellow",
+      theme = "dragon", -- Включение варианта dragon
+      background = {
+        dark = "dragon",
+      },
+      colors = {
+          theme = { all = { ui = { bg_gutter = 'none' }  }}
+        }
     },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
   },
 }
